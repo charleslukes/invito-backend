a072ade19b7c4bea9ce0698d2a2dfda7be2e6a8f
charleslukes <charleslukes28@gmail.com>
2023-10-02 22:12:16 +0100

    add user routes

---
+++ src/route.rs
@@ -0,0 +1,28 @@
+use std::sync::Arc;
+
+use axum::{
+    routing::{get, post},
+    Router,
+};
+
+use crate::{
+    handler::{
+        create_user_handler, delete_user_handler, edit_user_handler, get_user_handler,
+        health_checker_handler, users_list_handler,
+    },
+    AppState,
+};
+
+pub fn create_router(app_state: Arc<AppState>) -> Router {
+    Router::new()
+        .route("/api/healthchecker", get(health_checker_handler))
+        .route("/api/users/", post(create_user_handler))
+        .route("/api/users", get(users_list_handler))
+        .route(
+            "/api/user/:id",
+            get(get_user_handler)
+                .patch(edit_user_handler)
+                .delete(delete_user_handler),
+        )
+        .with_state(app_state)
+}
