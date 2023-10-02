137509ae27e5a60f545ff1ea641488da28df546a
charleslukes <charleslukes28@gmail.com>
2023-10-02 22:11:41 +0100

    add user schema

---
+++ src/schema.rs
@@ -0,0 +1,26 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Debug, Default)]
+pub struct FilterOptions {
+    pub page: Option<usize>,
+    pub limit: Option<usize>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct ParamOptions {
+    pub id: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct CreateUserSchema {
+    pub user_name: String,
+    pub email: String,
+    pub ref_code: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct UpdateUserSchema {
+    pub user_name: Option<String>,
+    pub email: Option<String>,
+    pub ref_code: Option<String>
+}
